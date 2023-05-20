6c48ec8601f7321309df1a41a25604885e4d5bd8
Darkee <debojitsantra48@gmail.com>
2023-05-20 17:29:18 +0530

    update utils/global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Urban Rahsoi';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : ' A Modern Indian Food Blog';
+    : 'A Modern Indian Food Blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : "2023 © Urban Rahsoi";
